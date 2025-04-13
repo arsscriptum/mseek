69eb123317b7a77a4c7b731d4116b575dabe12f4
Guillaume Plante <guillaume.plante@icloud.com>
2025-04-13 16:56:27 -0400

    set the extended scope branch version to 2.1.0

--- src/version.cpp
+++ src/version.cpp
@@ -11,17 +11,17 @@
 #include "version.h"
 
 #ifdef _RELEASE
-unsigned int mseek::version::major  = 1;
-unsigned int mseek::version::minor  = 2;
+unsigned int mseek::version::major  = 2;
+unsigned int mseek::version::minor  = 1;
 unsigned int mseek::version::build  = 0;
 unsigned int mseek::version::rev    = release;
-std::string  mseek::version::sha    = "main";
+std::string  mseek::version::sha    = "extended_scope_main";
 std::string  mseek::version::branch = "bfcdb109";
 #else
-unsigned int mseek::version::major  = 1;
-unsigned int mseek::version::minor  = 2;
+unsigned int mseek::version::major  = 2;
+unsigned int mseek::version::minor  = 1;
 unsigned int mseek::version::build  = 0;
-unsigned int mseek::version::rev    = 247;
-std::string  mseek::version::sha    = "main";
+unsigned int mseek::version::rev    = 0;
+std::string  mseek::version::sha    = "extended_scope_main";
 std::string  mseek::version::branch = "bfcdb109";
 #endif // _RELEASE
